7ca4af2d9e89a1f079719196859d1efe0e7b6263
seonrizee <seonrizee@gmail.com>
2025-10-15 16:06:17 +0900

    feat: Member 엔티티에 signUp 정적 팩토리 메서드 추가

--- src/main/java/com/sparta/couponpop/domain/member/entity/Member.java
+++ src/main/java/com/sparta/couponpop/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ import com.sparta.couponpop.common.entity.BaseEntity;
 import com.sparta.couponpop.domain.member.enums.MemberType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,4 +32,23 @@ public class Member extends BaseEntity {
     private MemberType memberType;
 
     private LocalDateTime deletedAt;
+
+    @Builder(access = AccessLevel.PROTECTED)
+    public Member(String email, String username, String password, String phoneNumber, MemberType memberType) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.memberType = memberType;
+    }
+
+    public static Member signUp(String email, String username, String password, String phoneNumber, MemberType memberType) {
+        return Member.builder()
+                .email(email)
+                .username(username)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .memberType(memberType)
+                .build();
+    }
 }
