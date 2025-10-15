ea6c74cd58ba087634975c478f23f3bfa6f98a6a
seyoung5744 <seyoung574458@gmail.com>
2025-10-15 09:40:30 +0900

    create init project

---
+++ src/main/java/com/sparta/couponpop/CouponPopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.couponpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponPopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponPopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.couponpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponPopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
