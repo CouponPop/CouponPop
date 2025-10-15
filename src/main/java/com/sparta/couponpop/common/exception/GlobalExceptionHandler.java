0048aed12dfcae1c9efcd7992370b1337274eb17
seonrizee <seonrizee@gmail.com>
2025-10-15 20:04:22 +0900

    feat: GlobalExceptionHandler에 유효성 검사 예외 핸들러 추가

--- src/main/java/com/sparta/couponpop/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/couponpop/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +19,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiErrorResponse> handleGlobalException(GlobalException ex, HttpServletRequest request) {
         log.error("비즈니스 오류 발생 ", ex);
         return handleExceptionInternal(ex.getErrorCode(), request);
+    }
+
+    // @Valid 실패 시 발생하는 예외를 처리, message가 여러 개일 수 있으므로 StringBuilder로 묶어서 반환
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+
+        StringBuilder errorsMessages = new StringBuilder();
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errorsMessages.append(fieldError.getDefaultMessage()).append("\n");
+        }
+
+        return handleExceptionInternal(HttpStatus.BAD_REQUEST, errorsMessages.toString(), request);
     }
 
     private ResponseEntity<ApiErrorResponse> handleExceptionInternal(ErrorCode errorCode, HttpServletRequest request) {
