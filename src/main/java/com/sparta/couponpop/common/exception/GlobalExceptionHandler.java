79af5f946ff922cfcc6c4762dbd02ceb273cd53f
seonrizee <seonrizee@gmail.com>
2025-10-16 11:09:59 +0900

    refactor: MethodArgumentNotValidException의 message 처리 개선

--- src/main/java/com/sparta/couponpop/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/couponpop/common/exception/GlobalExceptionHandler.java
@@ -3,13 +3,14 @@ package com.sparta.couponpop.common.exception;
 import com.sparta.couponpop.common.response.ApiErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static java.util.stream.Collectors.joining;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,19 +22,15 @@ public class GlobalExceptionHandler {
         return handleExceptionInternal(ex.getErrorCode(), request);
     }
 
-    // @Valid 실패 시 발생하는 예외를 처리, message가 여러 개일 수 있으므로 StringBuilder로 묶어서 반환
+    // @Valid 실패 시 발생하는 예외를 처리, message가 여러 개일 수 있으므로 묶어서 반환
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
 
-        StringBuilder errorsMessages = new StringBuilder();
+        String errorsMessages = ex.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(joining("\n"));
 
-        BindingResult bindingResult = ex.getBindingResult();
-
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            errorsMessages.append(fieldError.getDefaultMessage()).append("\n");
-        }
-
-        return handleExceptionInternal(HttpStatus.BAD_REQUEST, errorsMessages.toString(), request);
+        return handleExceptionInternal(HttpStatus.BAD_REQUEST, errorsMessages, request);
     }
 
     private ResponseEntity<ApiErrorResponse> handleExceptionInternal(ErrorCode errorCode, HttpServletRequest request) {
