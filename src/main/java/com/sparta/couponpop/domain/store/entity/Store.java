06582e2ae57c9411879c4151356e7e85fb4e9e65
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-10-16 10:04:33 +0900

    feat: implement factory method for Store entity

--- src/main/java/com/sparta/couponpop/domain/store/entity/Store.java
+++ src/main/java/com/sparta/couponpop/domain/store/entity/Store.java
@@ -4,6 +4,7 @@ import com.sparta.couponpop.common.entity.BaseEntity;
 import com.sparta.couponpop.domain.store.enums.StoreCategory;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -48,4 +49,45 @@ public class Store extends BaseEntity {
     private LocalTime weekendCloseTime;
 
     private LocalDateTime deletedAt;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Store(String name, String phone, String description, String businessNumber,
+                 String address, double latitude, double longitude, String imageUrl,
+                 StoreCategory storeCategory, LocalTime weekdayOpenTime, LocalTime weekdayCloseTime,
+                 LocalTime weekendOpenTime, LocalTime weekendCloseTime) {
+        this.name = name;
+        this.phone = phone;
+        this.description = description;
+        this.businessNumber = businessNumber;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.imageUrl = imageUrl;
+        this.storeCategory = storeCategory;
+        this.weekdayOpenTime = weekdayOpenTime;
+        this.weekdayCloseTime = weekdayCloseTime;
+        this.weekendOpenTime = weekendOpenTime;
+        this.weekendCloseTime = weekendCloseTime;
+    }
+
+    public static Store createStore(String name, String phone, String description, String businessNumber,
+                                  String address, double latitude, double longitude, String imageUrl,
+                                  StoreCategory storeCategory, LocalTime weekdayOpenTime, LocalTime weekdayCloseTime,
+                                  LocalTime weekendOpenTime, LocalTime weekendCloseTime) {
+        return Store.builder()
+                .name(name)
+                .phone(phone)
+                .description(description)
+                .businessNumber(businessNumber)
+                .address(address)
+                .latitude(latitude)
+                .longitude(longitude)
+                .imageUrl(imageUrl)
+                .storeCategory(storeCategory)
+                .weekdayOpenTime(weekdayOpenTime)
+                .weekdayCloseTime(weekdayCloseTime)
+                .weekendOpenTime(weekendOpenTime)
+                .weekendCloseTime(weekendCloseTime)
+                .build();
+    }
 }
