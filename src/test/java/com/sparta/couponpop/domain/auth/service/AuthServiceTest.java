391f43815fde10f02da22fa6f29ea3bfcce58a68
seonrizee <seonrizee@gmail.com>
2025-10-16 09:10:28 +0900

    fix: AuthServiceTest에 PasswordEncoder mock 추가

--- src/test/java/com/sparta/couponpop/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/sparta/couponpop/domain/auth/service/AuthServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,6 +25,9 @@ class AuthServiceTest {
 
     @Mock
     private MemberServiceApi memberService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private AuthService authService;
