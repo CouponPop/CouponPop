9f1c70426ac2dcb242b9672a023272a8ebd60be8
seonrizee <seonrizee@gmail.com>
2025-10-16 11:07:32 +0900

    feat: email unique 제약 조건으로 발생할 수 있는 exception에 대한 처리 추가

--- src/main/java/com/sparta/couponpop/domain/member/service/MemberService.java
+++ src/main/java/com/sparta/couponpop/domain/member/service/MemberService.java
@@ -5,6 +5,7 @@ import com.sparta.couponpop.domain.member.entity.Member;
 import com.sparta.couponpop.domain.member.exception.MemberErrorCode;
 import com.sparta.couponpop.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,13 +13,27 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService implements MemberServiceApi {
 
+    private static final String DUPLICATION_ERROR_TARGET = "email";
+
     private final MemberRepository memberRepository;
 
     @Transactional
     public Member createMember(Member newMember) {
-        if (memberRepository.existsByEmail(newMember.getEmail()))
-            throw new GlobalException(MemberErrorCode.EMAIL_DUPLICATED);
 
-        return memberRepository.save(newMember);
+        if (memberRepository.existsByEmail(newMember.getEmail())) {
+            throw new GlobalException(MemberErrorCode.EMAIL_DUPLICATED);
+        }
+
+        try {
+            Member savedMember = memberRepository.save(newMember);
+            memberRepository.flush(); // 제약조건 즉시 검증을 위해 사용
+            return savedMember;
+        } catch (DataIntegrityViolationException e) {
+            if (e.getMostSpecificCause().getMessage().contains(DUPLICATION_ERROR_TARGET)) {
+                throw new GlobalException(MemberErrorCode.EMAIL_DUPLICATED);
+            }
+
+            throw e;
+        }
     }
 }
