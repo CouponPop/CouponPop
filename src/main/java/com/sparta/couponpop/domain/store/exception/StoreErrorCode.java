e8256c8e8825981a91b946a7d1283c93397e7c7b
4x2vk <kuandykkaiyrzhan@gmail.com>
2025-10-17 17:34:17 +0900

    feat: add STORE_ALREADY_DELETED error code

--- src/main/java/com/sparta/couponpop/domain/store/exception/StoreErrorCode.java
+++ src/main/java/com/sparta/couponpop/domain/store/exception/StoreErrorCode.java
@@ -11,6 +11,7 @@ public enum StoreErrorCode implements ErrorCode {
 
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다."),
     STORE_NOT_FOUND(HttpStatus.BAD_REQUEST, "매장을 찾을 수 없습니다."),
+    STORE_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 삭제된 매장입니다."),
     STORE_UPDATE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "매장 수정 권한이 없습니다."),
     STORE_DELETE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "매장 삭제 권한이 없습니다.");
 
