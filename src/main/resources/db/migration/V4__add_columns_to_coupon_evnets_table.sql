e35df7ece17aefe3e02dc954b10d6c660d8cdc7a
seyoung5744 <seyoung574458@gmail.com>
2025-10-17 14:53:20 +0900

    refactor : issued_count 기본값 설정

--- src/main/resources/db/migration/V4__add_columns_to_coupon_evnets_table.sql
+++ src/main/resources/db/migration/V4__add_columns_to_coupon_evnets_table.sql
@@ -5,7 +5,7 @@ alter table coupon_events
     ADD COLUMN event_start_at       DATETIME            NOT NULL COMMENT '이벤트 시작 시간',
     ADD COLUMN event_end_at         DATETIME            NOT NULL COMMENT '이벤트 종료 시간',
     ADD COLUMN total_count          INT                 NOT NULL COMMENT '발급 가능한 쿠폰 총 수량',
-    ADD COLUMN issued_count         INT                 NOT NULL COMMENT '발금된 쿠폰 수량',
+    ADD COLUMN issued_count         INT                 NOT NULL DEFAULT 0 COMMENT '발금된 쿠폰 수량',
     ADD COLUMN created_at           DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
     ADD COLUMN updated_at           DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP ON
     UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
