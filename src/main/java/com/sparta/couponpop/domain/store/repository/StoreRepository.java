9d311115047f90929dfaeffc85a961eceb51d8e8
4x2vk <kuandykkaiyrzhan@gmail.com>
2025-10-17 13:24:57 +0900

    feat: add method findByMemberIdOrderByCreatedAtDesc to StoreRepository

--- src/main/java/com/sparta/couponpop/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/couponpop/domain/store/repository/StoreRepository.java
@@ -3,6 +3,10 @@ package com.sparta.couponpop.domain.store.repository;
 import com.sparta.couponpop.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    
+    List<Store> findByMemberIdOrderByCreatedAtDesc(Long memberId);
 }
 
