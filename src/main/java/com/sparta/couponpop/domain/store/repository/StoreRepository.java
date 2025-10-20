6b71df8b1749f1c6723de0b250371c8d1d092f1b
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-10-20 19:08:27 +0900

    fix: update query in StoreRepository

--- src/main/java/com/sparta/couponpop/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/couponpop/domain/store/repository/StoreRepository.java
@@ -73,6 +73,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
      * 매장명으로 매장을 검색합니다.
      * 삭제되지 않은 매장 중에서 매장명에 키워드가 포함된 매장을 조회합니다
      */
-    @Query("SELECT s FROM Store s WHERE s.name LIKE %:keyword% ORDER BY s.name ASC")
+    @Query("SELECT s FROM Store s WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword, '%')) ORDER BY s.name ASC")
     List<Store> findByNameContainingIgnoreCase(@Param("keyword") String keyword);
 }
