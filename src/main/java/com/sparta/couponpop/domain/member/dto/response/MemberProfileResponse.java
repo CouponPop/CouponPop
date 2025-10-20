d464c6642e167c0ed93d71d13380999409e9bf4a
seonrizee <seonrizee@gmail.com>
2025-10-20 14:45:08 +0900

    feat: Member 프로필 조회 API 구현

---
+++ src/main/java/com/sparta/couponpop/domain/member/dto/response/MemberProfileResponse.java
@@ -0,0 +1,22 @@
+package com.sparta.couponpop.domain.member.dto.response;
+
+import com.sparta.couponpop.domain.member.entity.Member;
+import com.sparta.couponpop.domain.member.enums.MemberType;
+
+public record MemberProfileResponse(
+        Long userId,
+        String username,
+        String email,
+        String phoneNumber,
+        MemberType memberType
+) {
+    public static MemberProfileResponse from(Member member) {
+        return new MemberProfileResponse(
+                member.getId(),
+                member.getUsername(),
+                member.getEmail(),
+                member.getPhoneNumber(),
+                member.getMemberType()
+        );
+    }
+}
