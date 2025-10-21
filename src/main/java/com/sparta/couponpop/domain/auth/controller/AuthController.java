48e0cd9211836337b49c1d02a9c03d310372eba2
seonrizee <seonrizee@gmail.com>
2025-10-21 16:58:13 +0900

    feat: FCM Token 만료 서비스 로직 추가

--- src/main/java/com/sparta/couponpop/domain/auth/controller/AuthController.java
+++ src/main/java/com/sparta/couponpop/domain/auth/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.sparta.couponpop.domain.auth.controller;
 
 import com.sparta.couponpop.common.response.ApiResponse;
+import com.sparta.couponpop.common.security.annotation.CurrentMember;
+import com.sparta.couponpop.common.security.dto.AuthMember;
 import com.sparta.couponpop.domain.auth.dto.request.LoginRequest;
+import com.sparta.couponpop.domain.auth.dto.request.LogoutRequest;
 import com.sparta.couponpop.domain.auth.dto.request.SignUpRequest;
 import com.sparta.couponpop.domain.auth.dto.response.LoginResponse;
 import com.sparta.couponpop.domain.auth.dto.response.SignUpResponse;
@@ -37,12 +40,11 @@ public class AuthController {
 
     @PostMapping("/logout")
     @PreAuthorize("isAuthenticated()") // auth는 모두 접근이므로, 로그아웃은 인증된 사용자만 접근 가능
-    public ResponseEntity<ApiResponse<Void>> logout(@RequestHeader("Authorization") String authorizationHeader) {
+    public ResponseEntity<ApiResponse<Void>> logout(@RequestHeader("Authorization") String authorizationHeader,
+                                                    @Valid @RequestBody LogoutRequest logoutRequest,
+                                                    @CurrentMember AuthMember authMember) {
 
-        authService.logout(authorizationHeader);
+        authService.logout(authorizationHeader, logoutRequest, authMember);
         return ApiResponse.noContent();
     }
-
-    // TODO: 로그아웃 시 FCM 토큰 삭제 기능 추가 예정
-    // TODO: https://github.com/orgs/CouponPop/projects/1/views/2?pane=issue&itemId=134369719&issue=CouponPop%7CCouponPop%7C61
 }
--- src/main/java/com/sparta/couponpop/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/couponpop/domain/auth/service/AuthService.java
@@ -2,20 +2,26 @@ package com.sparta.couponpop.domain.auth.service;
 
 import com.sparta.couponpop.common.exception.GlobalException;
 import com.sparta.couponpop.common.security.JwtProvider;
+import com.sparta.couponpop.common.security.dto.AuthMember;
 import com.sparta.couponpop.domain.auth.dto.request.LoginRequest;
+import com.sparta.couponpop.domain.auth.dto.request.LogoutRequest;
 import com.sparta.couponpop.domain.auth.dto.request.SignUpRequest;
 import com.sparta.couponpop.domain.auth.dto.response.LoginResponse;
 import com.sparta.couponpop.domain.auth.dto.response.SignUpResponse;
 import com.sparta.couponpop.domain.auth.exception.AuthErrorCode;
 import com.sparta.couponpop.domain.member.entity.Member;
+import com.sparta.couponpop.domain.member.entity.MemberFcmToken;
 import com.sparta.couponpop.domain.member.exception.MemberErrorCode;
+import com.sparta.couponpop.domain.member.repository.MemberFcmTokenRepository;
 import com.sparta.couponpop.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
@@ -24,6 +30,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
     private final TokenBlacklistService tokenBlacklistService;
+    private final MemberFcmTokenRepository memberFcmTokenRepository;
 
     @Transactional
     public SignUpResponse signUp(SignUpRequest signUpRequest) {
@@ -72,9 +79,13 @@ public class AuthService {
         return LoginResponse.from(accessToken);
     }
 
-    public void logout(String authorizationHeader) {
+    // TODO: JWT Token은 Transactional 영향 받지 않으므로, TransactionalEventListener 등 으로 원자적 처리 고려
+    // TODO: 다만 두 작업이 원자적으로 처리되어야 할 필요성이 있는지는 고민 필요
+    @Transactional
+    public void logout(String authorizationHeader, LogoutRequest logoutRequest, AuthMember authMember) {
 
         expireToken(authorizationHeader);
+        expireFcmToken(authMember.id(), logoutRequest.fcmToken());
     }
 
     // 로그아웃, 회원 탈퇴 시 블랙리스트 추가하여 토큰 만료 처리
@@ -88,5 +99,14 @@ public class AuthService {
         long expirationMillis = jwtProvider.getExpirationMillis(token);
 
         tokenBlacklistService.blacklistToken(token, expirationMillis);
+    }
+
+    private void expireFcmToken(Long memberId, String fcmToken) {
+
+        MemberFcmToken memberFcmToken = memberFcmTokenRepository
+                .findByMemberIdAndFcmToken(memberId, fcmToken)
+                .orElseThrow(() -> new GlobalException(MemberErrorCode.MEMBER_FCM_TOKEN_NOT_FOUND));
+
+        memberFcmTokenRepository.delete(memberFcmToken);
     }
 }
