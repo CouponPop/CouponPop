b8c139f68f572a7307594a685cbfd9eac8f412f3
seonrizee <seonrizee@gmail.com>
2025-10-27 18:06:35 +0900

    chore: openApi 상대주소 사용으로 변경

--- src/test/java/com/sparta/couponpop/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/sparta/couponpop/domain/member/controller/MemberControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -32,7 +31,6 @@ import org.springframework.test.web.servlet.ResultActions;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willThrow;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -90,7 +88,6 @@ class MemberControllerTest {
 
         // when
         ResultActions resultActions = mockMvc.perform(get("/api/v1/members/me")
-                .header(HttpHeaders.AUTHORIZATION, mockJwtToken)
                 .contentType(MediaType.APPLICATION_JSON));
 
         // then
@@ -109,9 +106,6 @@ class MemberControllerTest {
                                 .description("내 프로필 조회 API")
                                 .summary("사용자가 자신의 프로필 조회를 수행합니다.")
                                 .tag("member")
-                                .requestHeaders(
-                                        headerWithName(HttpHeaders.AUTHORIZATION).description("JWT 액세스 토큰 (예: `Bearer 64K07J2867Cw7JuA7Lqg7ZSE7L+g7Y+w7Yyd7JeR7IS47Iqk7Yag7YGw7Iuc7YGs66a/7YKk`)")
-                                )
                                 .responseFields(
                                         fieldWithPath("data.id").description("회원 ID"),
                                         fieldWithPath("data.username").description("사용자 이름"),
