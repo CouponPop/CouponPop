79a83d63d07721b01dc2eae1136c758904a9615f
seyoung5744 <seyoung574458@gmail.com>
2025-10-28 17:13:27 +0900

    test: SpringBootTest CouponUsageRepository Mock 빈 등록

--- src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
+++ src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
@@ -1,10 +1,16 @@
 package com.sparta.couponpop;
 
+import com.sparta.couponpop.common.elasticsearch.repository.CouponUsageRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CouponPopApplicationTests {
+
+    @Mock
+    private CouponUsageRepository couponUsageRepository;
 
     @Test
     void contextLoads() {
