66f99ef374ee1e96abf4ff6a9980468b46d7d6b1
seyoung5744 <seyoung574458@gmail.com>
2025-10-28 17:31:27 +0900

    test : 테스트 환경 ES 자동 구성 비활성화

--- src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
+++ src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
@@ -1,18 +1,10 @@
 package com.sparta.couponpop;
 
-import com.sparta.couponpop.common.elasticsearch.repository.CouponUsageRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class CouponPopApplicationTests {
-
-    @Mock
-    private CouponUsageRepository couponUsageRepository;
 
     @Test
     void contextLoads() {
