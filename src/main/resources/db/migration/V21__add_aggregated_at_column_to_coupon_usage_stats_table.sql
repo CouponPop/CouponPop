8df9abaebea0cdb719fb0d435f66b9e5f9f1649a
younghunkimm <hunifive@gmail.com>
2025-10-29 12:16:46 +0900

    fix: 테이블에 기존 데이터가 있는 경우 마이그레이션 실패 방지

--- src/main/resources/db/migration/V21__add_aggregated_at_column_to_coupon_usage_stats_table.sql
+++ src/main/resources/db/migration/V21__add_aggregated_at_column_to_coupon_usage_stats_table.sql
@@ -1,2 +1,12 @@
+-- aggregated_at 컬럼을 먼저 NULL 허용으로 추가
 ALTER TABLE coupon_usage_stats
-    ADD COLUMN aggregated_at DATE NOT NULL COMMENT '집계 날짜';
+    ADD COLUMN aggregated_at DATE NULL;
+
+-- 기존 데이터에 대해 aggregated_at 컬럼을 생성일 -1일로 채우기
+UPDATE coupon_usage_stats
+SET aggregated_at = DATE_SUB(created_at, INTERVAL 1 DAY)
+WHERE aggregated_at IS NULL;
+
+-- 컬럼을 NOT NULL로 변경
+ALTER TABLE coupon_usage_stats
+    MODIFY COLUMN aggregated_at DATE NOT NULL COMMENT '집계 날짜';
