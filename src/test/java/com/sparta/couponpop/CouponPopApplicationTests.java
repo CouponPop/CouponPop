ef16c64b49db642996e92f0eb78ce49e7067ff72
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-10-29 21:24:30 +0900

    test: add MockitoBean for StoreSearchRepository

--- src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
+++ src/test/java/com/sparta/couponpop/CouponPopApplicationTests.java
@@ -1,10 +1,15 @@
 package com.sparta.couponpop;
 
+import com.sparta.couponpop.domain.store.repository.StoreSearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CouponPopApplicationTests {
+
+    @MockitoBean
+    private StoreSearchRepository storeSearchRepository;
 
     @Test
     void contextLoads() {
