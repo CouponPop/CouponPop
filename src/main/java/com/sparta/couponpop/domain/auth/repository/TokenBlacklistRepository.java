9de3be18b6d917ba01deeef1ecbe065d11fc24dc
seonrizee <seonrizee@gmail.com>
2025-10-29 13:14:21 +0900

    feat: Redis 기반 토큰 블랙리스트 로직 Repository 구현 및 불필요한 클래스, 메서드 제거

--- src/main/java/com/sparta/couponpop/domain/auth/repository/TokenBlacklistRepository.java
+++ src/main/java/com/sparta/couponpop/domain/auth/repository/TokenBlacklistRepository.java
@@ -1,12 +1,12 @@
 package com.sparta.couponpop.domain.auth.repository;
 
+import java.time.format.DateTimeFormatter;
+
 public interface TokenBlacklistRepository {
+
+    DateTimeFormatter FORMATTER_YYYY_MM_DD_HH_MM_SS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     void save(String token, long expirationMillis);
 
     boolean exists(String token);
-
-    void deleteAllExpired(long currentTimeMillis);
-
-    int count();
 }
