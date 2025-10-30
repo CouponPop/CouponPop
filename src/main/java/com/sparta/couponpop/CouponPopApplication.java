13abda6515d40837e155be5926ae8174feaedc65
4x2vk <kuandykkaiyrzhan@gmail.com>
2025-10-30 16:47:10 +0900

    chore: remove unused ES repository annotation

--- src/main/java/com/sparta/couponpop/CouponPopApplication.java
+++ src/main/java/com/sparta/couponpop/CouponPopApplication.java
@@ -2,12 +2,10 @@ package com.sparta.couponpop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-@EnableElasticsearchRepositories(basePackages = "com.sparta.couponpop.domain.store.repository")
 public class CouponPopApplication {
 
     public static void main(String[] args) {
