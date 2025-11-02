b08564fc8a7f7153ad3ed746db53954a89999164
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-11-02 20:45:27 +0900

    refactor: rename MemberDtoResponse to MemberResponse

--- src/main/java/com/sparta/couponpop/common/dto/member/response/MemberResponse.java
+++ src/main/java/com/sparta/couponpop/common/dto/member/response/MemberResponse.java
@@ -7,12 +7,12 @@ import com.sparta.couponpop.domain.member.entity.Member;
  * 요청 도메인: store
  * 담당자: (담당자 이름)
  */
-public record MemberDtoResponse(
+public record MemberResponse(
         Long id,
         String username
 ) {
-    public static MemberDtoResponse from(Member member) {
-        return new MemberDtoResponse(
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
                 member.getId(),
                 member.getUsername()
         );
