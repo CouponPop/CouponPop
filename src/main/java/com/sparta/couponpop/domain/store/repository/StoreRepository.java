81001c6807e40ac7f95a3ce7a9c3df146b6cdbc4
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-11-03 16:44:38 +0900

    feat: add cursor based pagination methods to StoreRepository

--- src/main/java/com/sparta/couponpop/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/couponpop/domain/store/repository/StoreRepository.java
@@ -2,6 +2,7 @@ package com.sparta.couponpop.domain.store.repository;
 
 import com.sparta.couponpop.domain.store.entity.Store;
 import jakarta.persistence.QueryHint;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
@@ -27,6 +28,18 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
      * 최신 생성 순으로 정렬하여 반환합니다.
      */
     List<Store> findByMemberIdOrderByCreatedAtDesc(Long memberId);
+
+    /**
+     * 회원 ID로 매장 목록을 조회합니다 (ID 내림차순).
+     * cursor 기반 페이징의 첫 페이지 조회에 사용.
+     */
+    List<Store> findByMemberIdOrderByIdDesc(Long memberId, Pageable pageable);
+
+    /**
+     * 회원 ID로 매장 목록을 조회합니다 (ID 내림차순, 지정된 ID보다 작은 값).
+     * cursor 기반 페이징의 다음 페이지 조회에 사용.
+     */
+    List<Store> findByMemberIdAndIdLessThanOrderByIdDesc(Long memberId, Long lastStoreId, Pageable pageable);
 
     /**
      * 모든 매장을 스트림으로 조회합니다.
